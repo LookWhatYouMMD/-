f5b15c9e4654d89104749c6dde63d48c19ba0b06
LookWhatYouMMD <37856468+LookWhatYouMMD@users.noreply.github.com>
2018-05-07 11:40:01 +0800

    2018.5.7

--- js5/DOM.js
+++ js5/DOM.js
@@ -2,5 +2,12 @@ function init(){
   var obj = document.getElementById("title");
   alert(obj.innerHTML);
   obj.innerHTML = "GOODBYE WORLD!!"
+  obj.setAttribute('class','tit');
+  picobj.getAttribute('alt');
+}
+
+function changePicture(){
+  picobj.setAttribute('scr','2.jpg');
 }
 window.onload = init;
+document.getElementById('title').innerHTML=a.getAttribute('666')
